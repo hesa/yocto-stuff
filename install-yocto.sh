9494619735dbe30e80f04ecdf4e86c514571a3d7
Sandklef Henrik <hesa@sandklef.com>
2021-06-18 09:51:51 +0200

    installing req optional

--- install-yocto.sh
+++ install-yocto.sh
@@ -18,6 +18,7 @@ exit_on_error()
 
 install_req()
 {
+    echo "Installing required software"
     sudo apt-get install gawk wget git-core diffstat unzip texinfo gcc-multilib build-essential chrpath socat cpio python3 python3-pip python3-pexpect xz-utils debianutils iputils-ping python3-git python3-jinja2 libegl1-mesa libsdl1.2-dev pylint3 xterm python3-subunit mesa-common-dev
     exit_on_error $? "apt-get install"
 }
@@ -94,7 +95,11 @@ build()
     exit_on_error $? "bitbake core-image-minimal"
 }
 
-install_req
+
+if [ "$1" = "--install-requirements" ]
+then
+    install_req
+fi
 
 get_poky
 
